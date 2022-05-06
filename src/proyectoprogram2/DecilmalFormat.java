0e7d48132c3558fc428a994b3840ec2b8d0ca7d5
Juan Madrid <72727166+JrMadrid@users.noreply.github.com>
2022-05-05 23:27:17 -0600

    subiendo proyecto programacion 2

---
+++ src/proyectoprogram2/DecilmalFormat.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogram2;
+
+/**
+ *
+ * @author Juan Madrid
+ */
+class DecilmalFormat {
+    
+}
---
+++ src/proyectoprogram2/ProyectoProgram2.java
@@ -0,0 +1,18 @@
+/*
+Proyecto para la 4 unidad de programacion 2
+Juan Ramón Madrid 20172000761
+ */
+package proyectoprogram2;
+
+/**
+ *
+ * @author Juan Madrid
+ */
+public class ProyectoProgram2 {
+
+    public static void main(String[] args) {
+        PM pm1 = new PM();
+        pm1.setVisible(true);
+    }
+    
+}
